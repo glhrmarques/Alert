87c90d5219fd4b65312bcfd1117f47283b7e9d8a
glhrmarques <glhrmarques@hotmail.com>
2021-11-18 14:55:44 -0300

    Main page app

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, Text, StyleSheet, View, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+export default function App(){
+  //Alert
+  const cardAlert = () => Alert.alert( "Title", "Text",
+    [
+      {text:'Ok', onPress: () => console.log('Ok button has been pressed')},
+      {text:'Cancel', onPress: () => console.log('Cancel button has been pressed')}
+    ]
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <View style={{ flex:1, justifyContent:'center', alignItems:'center'}}>
+      <Pressable style={{padding: 24, backgroundColor: '#3d3d3d', borderRadius:8}} onPress={cardAlert} hitSlip={50}>
+        <Text style={{color: '#fafafa', fontWeight: 'bold'}}>Something</Text>
+      </Pressable>
+    </View>
+  )
+}
